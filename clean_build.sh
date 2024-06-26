3635f3419bd975eab65ea398b9f7cd9dcbeb866f
wxd <wxd@wxds-MacBook-Pro.local>
2024-06-26 23:53:27 +0800

    fix compile error
    Signed-off-by: wxd <wxd@wxds-MacBook-Pro.local>

--- clean_build.sh
+++ clean_build.sh
@@ -14,7 +14,8 @@ if [ "$(uname)" = "Darwin" ]; then
     # OSX needs a lot of extra help, poor thing
     # run the osx_environment.sh script to fix paths
     . ./osx_environment.sh
-    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 $B_CMAKE_FLAGS"
+    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/SDKs/MacOSX14.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=14.4 $B_CMAKE_FLAGS"
+    echo $B_CMAKE_FLAGS
 fi
 # allow local customizations to build environment
 [ -r ./build_env.sh ] && . ./build_env.sh
